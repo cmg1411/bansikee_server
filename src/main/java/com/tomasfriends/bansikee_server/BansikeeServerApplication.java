8f9277cc50aeaf8e34f1a5e5dfc575ee552c28d9
tomas <cmg1411@naver.com>
2021-02-21 16:51:36 +0900

    initial commit

---
+++ src/main/java/com/tomasfriends/bansikee_server/BansikeeServerApplication.java
@@ -0,0 +1,13 @@
+package com.tomasfriends.bansikee_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BansikeeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BansikeeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomasfriends/bansikee_server/BansikeeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomasfriends.bansikee_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BansikeeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
