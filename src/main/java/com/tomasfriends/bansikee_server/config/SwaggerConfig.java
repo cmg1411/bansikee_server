582a69fb00d9b1b4c3ee2d250e3f6c1bebff0b16
tomas <cmg1411@naver.com>
2021-02-28 03:39:29 +0900

    fix: Swagger 404 에러 수정

--- src/main/java/com/tomasfriends/bansikee_server/config/SwaggerConfig.java
+++ src/main/java/com/tomasfriends/bansikee_server/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.tomasfriends.bansikee_server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,7 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket api() {
@@ -31,5 +33,11 @@ public class SwaggerConfig {
             .license("TomasFriends")
             .version("1.0.0")
             .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html") .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**") .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
