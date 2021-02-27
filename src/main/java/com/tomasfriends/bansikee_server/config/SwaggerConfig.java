d5108ce49fd9360e4b3989cc0c60e518c171dada
tomas <cmg1411@naver.com>
2021-02-28 03:08:57 +0900

    feat: Swagger API 문서 자동화

---
+++ src/main/java/com/tomasfriends/bansikee_server/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.tomasfriends.bansikee_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Bansikee Server API Documentation")
+            .description("반식이 앱 REST API 연동 명세 문서입니다.")
+            .license("TomasFriends")
+            .version("1.0.0")
+            .build();
+    }
+}
--- src/main/java/com/tomasfriends/bansikee_server/controller/HelloController.java
+++ src/main/java/com/tomasfriends/bansikee_server/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.tomasfriends.bansikee_server.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Api(tags = {"1. User"})
 @Controller
 public class HelloController {
 
+    @ApiOperation(value = "회원 조회", notes = "모든 회원 조회")
     @GetMapping("/")
     public String hello(Model model) {
         model.addAttribute("data", "hello~!! 이제 된다 !! 진짜 된다 ! 슬렉 알림도 ! 데이터베이스 연동도 !");
