5afe3dc18415db931731b8fa12201873ce28c50d
tomas <cmg1411@naver.com>
2021-03-09 20:04:21 +0900

    fix: conflict 해결

--- src/main/java/com/tomasfriends/bansikee_server/response/dto/SuccessCode.java
+++ src/main/java/com/tomasfriends/bansikee_server/response/dto/SuccessCode.java
@@ -13,7 +13,14 @@ public enum SuccessCode {
     DELETE_USER_SUCCESS(225, "Success delete User.", "회원탈퇴에 성공했습니다."),
     GET_IS_ONBOARDED_SUCCESS(230, "Success get isOnBoarded.", "온보딩 여부를 반환했습니다."),
     NICKNAME_CHECK_SUCCESS(223, "Success to get nickName Duplication.", "닉네임 중복 검사를 성공했습니다."),
-    LOGIN_SUCCESS(219, "Login Success", "로그인에 성공했습니다.");
+    LOGIN_SUCCESS(219, "Login Success", "로그인에 성공했습니다."),
+
+    //----onBoarding
+    GET_ON_BOARDING_SUCCESS(200, "Get onBaording success", "설문지 조회 성공."),
+    ON_BOARDING_RESULT_SUCCESS(200, "Post onBaording result success", "설문 결과 보내기 성공."),
+    RECOMMEND_PLANT_SUCCESS(200, "Get recommendation success", "추천 식물 조회 성공."),
+    LIKE_SUCCESS(200, "Post like success", "좋아요 성공."),
+    UNDO_LIKE_SUCCESS(201, "Post undo like success", "좋아요 취소 성공.");
 
     private final int status;
     private final String title;
